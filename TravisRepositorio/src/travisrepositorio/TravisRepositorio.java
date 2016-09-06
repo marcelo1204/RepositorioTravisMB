c97e01d42fc33a841038d44f79588bfa90af40e4
Alberto <arnalpal95@gmail.com>
2016-09-05 21:43:06 -0500

    crear una variable y mostrar mensaje

--- TravisRepositorio/src/travisrepositorio/TravisRepositorio.java
+++ TravisRepositorio/src/travisrepositorio/TravisRepositorio.java
@@ -10,12 +10,19 @@ package travisrepositorio;
  * @author arnal
  */
 public class TravisRepositorio {
-
+    
+    private int uno;
     /**
      * @param args the command line arguments
      */
+    public void igual(){
+        if(uno!=0){
+            System.out.println("El numero es mayor a 0");
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
     
 }
